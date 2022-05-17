d142c5481831b0796c282fe8649a8f2cdd9cf95b
Benjamin Deininger <Benjamin.M.Deininger.ctr@nga.mil>
2022-05-16 21:02:02 -0600

    More changes

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const express = require("express");
+const path = require("path")
+
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(express.static(path.join(__dirname, './src')));
+const publicPath = path.join(__dirname, 'public');
+/*
+
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+})
+
+
+app.use(express.static(path.join(__dirname, '../build')))*/
+
+/*
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+})
+*/
+
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-
